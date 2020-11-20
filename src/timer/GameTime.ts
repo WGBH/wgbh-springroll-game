aa0c008c87a285cb4fd7ebb260c4922cfea18d76
Eric Ente <ericente@gmail.com>
2020-11-20 12:35:07 -0500

    update language in comments on GameTime

--- src/timer/GameTime.ts
+++ src/timer/GameTime.ts
@@ -1,10 +1,10 @@
 /**
  * 
- *  GameTime is a relay singleton that any object can hook into via its SpringRoll Property to get the next tick (gameTick) of the game clock.
+ *  GameTime is a relay singleton that any object can hook into via its static subscribe() method to get the next tick of the game clock.
  *  Its update() should be called on any live tick of the game; determining whether the tick is live (e.g. checking paused) should happen elsewhere.
  *  
- *  Call in the game's main tick/update function, using the singleton syntax on the class - GameTime.Instance.update(deltaTime);
- *  Subscribe to changes using singleton syntax on the class - GameTime.Instance.gameTick.subscribe(callbackfunction)
+ *  Call in the game's main tick/update function, using the static method on the class - GameTime.update(deltaTime);
+ *  Subscribe to changes using static method on the class - GameTime.subscribe(callbackfunction)
  *  
  */
 import { Property } from "springroll";
